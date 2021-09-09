45992cb5ee2ea2437bb5b4dc64a09c848e977c93
Florbela <flor20012005@gmail.com>
2021-09-09 15:04:46 -0300

    Projeto finalizado

--- src/area/Area.java
+++ src/area/Area.java
@@ -9,26 +9,12 @@ public class Area {
 		Scanner leitor = new Scanner(System.in);
 		
 		String nome, decisao = "S";
-		double largura, comprimento, areaComodo, areaTotal;
+		double largura, comprimento, areaComodo, areaTotal = 0;
 		
 		System.out.println("-----------------------------------------");
 		System.out.println("Cálculo da área de uma casa");
 		System.out.println("-----------------------------------------");
 		System.out.println();
-		
-		System.out.print("Qual o nome do cômodo? ");
-		nome = leitor.next();
-		System.out.print("Qual a largura? ");
-		largura = leitor.nextInt();
-		System.out.print("Qual o comprimento? ");
-		comprimento = leitor.nextInt();
-		
-		areaComodo = largura * comprimento;
-		
-		System.out.println("O(a) " + nome + " tem " + areaComodo + "metros quadrados." );
-		
-		System.out.print("Você deseja calcular mais cômodos(S/N)? ");
-		decisao = leitor.next();
 		
 		while(decisao.equals("S") || decisao.equals("s")) {
 			System.out.print("Qual o nome do cômodo? ");
@@ -40,13 +26,22 @@ public class Area {
 			
 			areaComodo = largura * comprimento;
 			
+			areaTotal = areaTotal + areaComodo;
+			
 			System.out.println("O(a) " + nome + " tem " + areaComodo + "metros quadrados." );
+			
+			System.out.println();
 			
 			System.out.print("Você deseja calcular mais cômodos(S/N)? ");
 			decisao = leitor.next();
+			
+			leitor.close();
 		}
 		
-		System.out.println("A área total da casa é " +  " metros quadrados");
+		System.out.println("A área total da casa é " + areaTotal +  " metros quadrados");
+		
+		System.out.println();
+		
 		System.out.println("**FIM**");
 	}
 
